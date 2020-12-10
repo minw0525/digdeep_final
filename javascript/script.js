ac2f1e3f3deea23a276cc7148281653ff6bc1e0c
minw0525 <74699528+minw0525@users.noreply.github.com>
2020-12-10 12:26:27 +0900

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -56,18 +56,18 @@ function checkUrl(url){
 	filePath = window.location.pathname;
 	function getFilePath(path){
 		switch (path) {
-			case "/":
-			//case "/Users/minuuuu/Google%20%EB%93%9C%EB%9D%BC%EC%9D%B4%EB%B8%8C/%ED%95%99%EA%B5%90/2020-2%20%EC%A1%B8%EC%97%85%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/%EC%A1%B8%EC%97%85%EC%A0%84%EC%8B%9C%EC%9B%B9%ED%8C%80/digdeep/index.html" :
+			//case "/":
+			case "/digdeep_final/" :
 				pageIdx = 1;
 				console.log(pageIdx)
 				return pageIdx;
 
-			case "/project":
+			case "/digdeep_final/project":
 				pageIdx = 2;
 				console.log(pageIdx);
 				return pageIdx;
 
-			case '/credit':
+			case "/digdeep_final/credit':
 				pageIdx = 3;
 				console.log(pageIdx)
 				return pageIdx;
@@ -79,8 +79,6 @@ function checkUrl(url){
 
 	getParam();
 	getFilePath(filePath);
-	pageIdx = 3;
-
 	console.log(currLang);
 	console.log(pageIdx);
 };
